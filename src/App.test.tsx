13e64740130a5eb6335e16d9afe07390e8952ce4
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-03-22 08:24:09 +0200

    small refactor

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,16 @@ import { screen, render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-    it('should render hello world', () => {
-        // Arrange
-        render(<App />);
-        const heading = 'Hello world!';
-        // Act
-        // Expect
-        expect(
-            screen.getByRole('heading', {
-                level: 1,
-            })
-        ).toHaveTextContent(heading);
-    });
+  it('should render hello world', () => {
+    // Arrange
+    render(<App />);
+    const heading = 'Hello world!';
+    // Act
+    // Expect
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      }),
+    ).toHaveTextContent(heading);
+  });
 });
