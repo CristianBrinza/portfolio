bfdf0983f7a02458ec4aa50d044b2547af98c815
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-08-09 16:55:08 +0300

    Project reinitialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import './App.css';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import { ThemeProvider } from './components/ThemeContext';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import WebsiteWarning from './components/WebiteWarning';
+
+import './App.css'
 
 function App() {
+
   return (
-    <ThemeProvider>
-      <WebsiteWarning />
-      <HashRouter>
-        <Routes>
-          <Route path="/:lang" element={<Home />} />
-          <Route path="/" element={<Navigate replace to="/en" />} />
-          {/* Default to English */}
-          <Route path="*" element={<NotFound />} />
-          <Route path="/:lang/contact" element={<Contact />} />
-          <Route path="/:lang/about" element={<About />} />
-        </Routes>
-      </HashRouter>
-    </ThemeProvider>
-  );
+    <>
+     hi
+    </>
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  // server: {
-  //   port: 3001, // Change the port to 3000
-  // },
-  base: 'https://cristianbrinza.github.io/portfolio/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'], // Optional: If you have global test setup file
-  },
-});
+})
