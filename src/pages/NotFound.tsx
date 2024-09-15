47289374761b75dd781e7089ff0454c81406f9cd
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-09-15 22:57:38 +0300

    Update Not Found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,6 +4,7 @@ import { Trans } from 'react-i18next';
 import Footer from '../components/Footer/Footer.tsx';
 import '../styles/NotFound.css';
 import Page from '../components/Page.tsx';
+import Icon from "../components/Icon.tsx";
 
 export default function NotFound() {
   const [pageHeight, setPageHeight] = useState('calc(100vh - 160px)');
@@ -31,13 +32,22 @@ export default function NotFound() {
   return (
     <>
       <Breadcrumb items={breadcrumbItems} />
-      <Page className="not_found_page" style={{ height: pageHeight }}>
+      <Page className="not_found_page" gap="20px" style={{height: pageHeight}}>
+      <Icon type="info" size="80px" color="var(--theme_primary_color_darkest_gray"/>
         <h1>
           404 <span id="not_found_page_dash">-</span>
-          <br id="not_found_page_br" /> Not Found
+          <br id="not_found_page_br"/> Not Found
         </h1>
+
+        <div className="not_found_page_under">
+          There’s no page at this address <br/>
+          Check the URL and try again, or use the search field to find what you need.
+          <br/><br/>
+
+          If there should be something here <a href="https://github.com/CristianBrinza/portfolio/issues/new?title=[cristianbrinza.com] 404 not found&amp;labels=polaris.shopify.com">let us know</a>.
+        </div>
       </Page>
-      <Footer />
+      <Footer/>
     </>
   );
 }
