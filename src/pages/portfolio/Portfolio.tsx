ccd12ff3b24ec7765a10e4f1a9ec087408f33b10
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-09-27 19:39:05 +0300

    Update Portfolio page

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -128,7 +128,7 @@ export default function Portfolio() {
           further inquiries, feel free to contact me.
         </div>
       </Page>
-      <Footer button={false} copyright={false} />
+      <Footer />
     </>
   );
 }
