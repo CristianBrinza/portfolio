2dbafb892cd8a63354800e17490bfb9ee150caad
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-10-16 16:03:15 +0300

    Update ShowCards.tsx

--- src/components/show_card/ShowCards.tsx
+++ src/components/show_card/ShowCards.tsx
@@ -46,86 +46,86 @@ const ShowCards: React.FC<ShowCardProps> = ({ items, id, className = '' }) => {
   };
 
   return (
-      <div id={id} className={`show_cards ${className}`}>
-        {items.slice(0, visibleCards).map((item: ShowCardItem, index: number) => {
-          return (
-              <div className="show_card" id={item.id} key={index}>
-                {item.img ? (
-                    <img src={item.img} alt="Portfolio" className="show_card_img" />
-                ) : (
-                    <div className="show_card_img_empty"></div>
-                )}
-                <div className="show_card_title">{item.title}</div>
+    <div id={id} className={`show_cards ${className}`}>
+      {items.slice(0, visibleCards).map((item: ShowCardItem, index: number) => {
+        return (
+          <div className="show_card" id={item.id} key={index}>
+            {item.img ? (
+              <img src={item.img} alt="Portfolio" className="show_card_img" />
+            ) : (
+              <div className="show_card_img_empty"></div>
+            )}
+            <div className="show_card_title">{item.title}</div>
 
-                {item.link || item.demo || item.more ? (
-                    <div className="show_card_btns">
-                      {item.link ? (
-                          <>
-                            <Button
-                                color="var(--theme_primary_color_black)"
-                                border="var(--theme_primary_color_dark_gray)"
-                                bgcolor="var(--theme_primary_color_dark_gray)"
-                                hover_bgcolor="var(--theme_primary_color_darkest_gray)"
-                                className="show_card_btn_link"
-                                to={item.link}
-                            >
-                              Source
-                            </Button>
-                          </>
-                      ) : null}
-                      {item.demo ? (
-                          <>
-                            <Button
-                                color="#ffffff"
-                                border="#1967d2"
-                                bgcolor="#1967d2"
-                                hover_bgcolor="#1559b7"
-                                hover_color="#ffffff"
-                                to={item.demo}
-                                className="show_card_btn_demo"
-                            >
-                              Demo
-                            </Button>
-                          </>
-                      ) : null}
-                      {item.more ? (
-                          <>
-                            <Button
-                                color="#ffffff"
-                                border="#1967d2"
-                                bgcolor="#1967d2"
-                                hover_bgcolor="#1559b7"
-                                hover_color="#ffffff"
-                                to={item.more}
-                                className="show_card_btn_demo"
-                            >
-                              More
-                            </Button>
-                          </>
-                      ) : null}
-                    </div>
-                ) : (
-                    <div className="show_card_subtitle_hiden">{item.description}</div>
-                )}
-
-                <div className="show_card_subtitle">{item.description}</div>
+            {item.link || item.demo || item.more ? (
+              <div className="show_card_btns">
+                {item.link ? (
+                  <>
+                    <Button
+                      color="var(--theme_primary_color_black)"
+                      border="var(--theme_primary_color_dark_gray)"
+                      bgcolor="var(--theme_primary_color_dark_gray)"
+                      hover_bgcolor="var(--theme_primary_color_darkest_gray)"
+                      className="show_card_btn_link"
+                      to={item.link}
+                    >
+                      Source
+                    </Button>
+                  </>
+                ) : null}
+                {item.demo ? (
+                  <>
+                    <Button
+                      color="#ffffff"
+                      border="#1967d2"
+                      bgcolor="#1967d2"
+                      hover_bgcolor="#1559b7"
+                      hover_color="#ffffff"
+                      to={item.demo}
+                      className="show_card_btn_demo"
+                    >
+                      Demo
+                    </Button>
+                  </>
+                ) : null}
+                {item.more ? (
+                  <>
+                    <Button
+                      color="#ffffff"
+                      border="#1967d2"
+                      bgcolor="#1967d2"
+                      hover_bgcolor="#1559b7"
+                      hover_color="#ffffff"
+                      to={item.more}
+                      className="show_card_btn_demo"
+                    >
+                      More
+                    </Button>
+                  </>
+                ) : null}
               </div>
-          );
-        })}
+            ) : (
+              <div className="show_card_subtitle_hiden">{item.description}</div>
+            )}
 
-        {items.length > visibleCards && (
-            <Button
-                color="var(--theme_primary_color_black)"
-                border="var(--theme_primary_color_dark_gray)"
-                bgcolor="var(--theme_primary_color_dark_gray)"
-                hover_bgcolor="var(--theme_primary_color_darkest_gray)"
-                className="show_more_button"
-                onClick={showMoreCards}
-            >
-              Show More
-            </Button>
-        )}
-      </div>
+            <div className="show_card_subtitle">{item.description}</div>
+          </div>
+        );
+      })}
+
+      {items.length > visibleCards && (
+        <Button
+          color="var(--theme_primary_color_black)"
+          border="var(--theme_primary_color_dark_gray)"
+          bgcolor="var(--theme_primary_color_dark_gray)"
+          hover_bgcolor="var(--theme_primary_color_darkest_gray)"
+          className="show_more_button"
+          onClick={showMoreCards}
+        >
+          Show More
+        </Button>
+      )}
+    </div>
   );
 };
 
